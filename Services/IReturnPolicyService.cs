c6a77d54ce509c5d55ef73c447042ebd3f27394a
DucTim <121352936+DucTim@users.noreply.github.com>
2024-06-14 14:33:42 +0700

    changes to services in policy and price display

---
+++ Services/IReturnPolicyService.cs
@@ -0,0 +1,13 @@
+﻿using BackEnd.Models;
+
+namespace BackEnd.Services
+{
+    public interface IReturnPolicyService
+    {
+        Task<ReturnPolicy> AddReturnPolicyAsync(ReturnPolicy returnPolicy);
+        Task<ReturnPolicy?> SearchReturnPolicyAsync(int? policyId);
+        Task<IEnumerable<ReturnPolicy>> GetAllReturnPoliciesAsync();
+        Task<bool> UpdateReturnPolicyAsync(int id, ReturnPolicy returnPolicy);
+        Task<bool> DeleteReturnPolicyAsync(int id);
+    }
+}
