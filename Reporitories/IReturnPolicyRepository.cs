13438a5d60227f92cfc3e92b8afb345a88b9d576
DucTim <121352936+DucTim@users.noreply.github.com>
2024-06-14 14:32:34 +0700

    Add files via upload

---
+++ Reporitories/IReturnPolicyRepository.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.Models;
+
+namespace BackEnd.Reporitories
+{
+    public interface IReturnPolicyRepository
+    {
+        Task<ReturnPolicy> AddReturnPolicyAsync(ReturnPolicy returnPolicy);
+        Task<ReturnPolicy?> GetReturnPolicybyId(int? id);
+        Task<IEnumerable<ReturnPolicy>> GetAllReturnPoliciesAsync();
+        Task<bool> DeleteReturnPolicyAsync(int id);
+        Task<bool> UpdateReturnPolicyAsync(int id, ReturnPolicy returnPolicy);
+
+    }
+}
